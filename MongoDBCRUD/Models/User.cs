0d5858a7103b9948a95966406ac6ab44d1338b88
Dipendra.Shekhawat <dipendra.shekhawat@greenorbit.com>
2019-10-03 11:21:10 +0530

    initial commit for ASP.NET Core Web application with MongoDB

---
+++ MongoDBCRUD/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+namespace MongoDBCRUD.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Age")]
+        public int Age { get; set; }
+
+        [BsonElement("Email")]
+        public int Email { get; set; }
+
+        [BsonElement("PhoneNumber")]
+        public int PhoneNumber { get; set; }
+
+        [BsonElement("Kids")]
+        public int Kids { get; set; }
+
+        [BsonElement("Home")]
+        public int Home { get; set; }
+    }
+}
---
+++ MongoDBCRUD/Services/UserService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using MongoDBCRUD.Models;
+using System.Collections.Generic;
+
+namespace MongoDBCRUD.Services
+{
+    public class UserService
+    {
+        private readonly IMongoCollection<User> users;
+
+        public UserService(IConfiguration config)
+        {
+            MongoClient client = new MongoClient(config.GetConnectionString("UserDetailsDB"));
+            IMongoDatabase database = client.GetDatabase("Users");
+            users = database.GetCollection<User>("users");
+        }
+
+        public List<User> Get()
+        {
+            return users.Find(car => true).ToList();
+        }
+
+        public User Get(string id)
+        {
+            return users.Find(car => car.Id == id).FirstOrDefault();
+        }
+    }
+}
